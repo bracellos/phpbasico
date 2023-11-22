ca178f471ccb1565a383cce08523c643a6f26189
Diego Bracellos <bracellos@gmail.com>
2023-11-21 22:07:11 -0300

    commit aula 1

---
+++ funcoes.php
@@ -0,0 +1,18 @@
+<?php
+
+//funções nativas do php
+
+$dias = [
+    "domingo",
+    "segunda",
+    "terça",
+    "quarta",
+    "quinta",
+    "sexta",
+    "sábado"
+];
+
+//implode junta itens de um array com um separador
+$listaDias = implode(" | ", $dias);
+
+?>
